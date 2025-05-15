fb19faaae6c4c3132e272330f526331d2016f388
ahmed1998 <nextelaaly6@gmail.com>
2025-05-15 17:32:31 +0100

    nedxt

--- pglocal.session.sql
+++ pglocal.session.sql
@@ -1,1 +1,21 @@
+CREATE TABLE Customer (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
 
+CREATE TABLE CustomerProfile (
+    id SERIAL PRIMARY KEY,
+    isLoggedIn BOOLEAN DEFAULT false,
+    customer_id INT UNIQUE NOT NULL,
+    CONSTRAINT fk_customer
+        FOREIGN KEY(customer_id) 
+        REFERENCES Customer(id)
+        ON DELETE CASCADE
+);
+
+-- Insert Customers
+INSERT INTO Customer (first_name, last_name) VALUES
+('John', 'Doe'),
+('Jerome', 'Lalu'),
+('Lea', 'Rive');
--- week2/day3/test.sql
+++ week2/day3/test.sql
@@ -53,3 +53,35 @@ VALUES
     (2, 2, 'Masterpiece of Space Exploration', 9, 'A stunning visual journey through the universe.');
 -- Delete a film that has a review from the new_film table, what happens to the customer_review table?
 DELETE FROM new_film WHERE id = 1;
+-- Exersice 2
+
+-- Use UPDATE to change the language of some films. Make sure that you use valid languages.
+
+UPDATE new_film 
+SET language_id = 1 
+WHERE id = 1;
+
+UPDATE new_film 
+SET language_id = 2 
+WHERE id = 2;
+
+UPDATE new_film 
+SET language_id = 3
+WHERE id = 3;
+-- Which foreign keys (references) are defined for the customer table? How does this affect the way in which we INSERT into the customer table?
+
+
+	SELECT 
+    customer.customer_id AS CustomerID,
+    customer.first_name AS FirstName,
+    customer.last_name AS LastName,
+    address.address AS Address,
+    address.district AS District,
+    store.manager_staff_id AS Store_name,
+    customer.last_update AS LastUpdate
+FROM 
+    customer
+JOIN 
+    address ON customer.address_id = address.address_id
+LEFT JOIN 
+    store ON customer.store_id = store.store_id;
