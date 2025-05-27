cb83147811efbd1b088dd4ce961d72eae18de8e0
ahmed1998 <nextelaaly6@gmail.com>
2025-05-27 17:18:58 +0100

    update

--- week2/day3/exersuse/test.sql
+++ week2/day3/exersuse/test.sql
@@ -57,21 +57,15 @@ DELETE FROM new_film WHERE id = 1;
 
 -- Use UPDATE to change the language of some films. Make sure that you use valid languages.
 
-UPDATE new_film 
-SET language_id = 1 
-WHERE id = 1;
+UPDATE film
+SET language_id = 1
+WHERE language_id = 2;
+-- check
+SELECT * FROM language;
 
-UPDATE new_film 
-SET language_id = 2 
-WHERE id = 2;
-
-UPDATE new_film 
-SET language_id = 3
-WHERE id = 3;
 -- Which foreign keys (references) are defined for the customer table? How does this affect the way in which we INSERT into the customer table?
-
-
-	SELECT 
+	
+    SELECT 
     customer.customer_id AS CustomerID,
     customer.first_name AS FirstName,
     customer.last_name AS LastName,
@@ -85,3 +79,62 @@ JOIN
     address ON customer.address_id = address.address_id
 LEFT JOIN 
     store ON customer.store_id = store.store_id;
+    -- . We created a new table called customer_review. Drop this table. Is this an easy step, or does it need extra checking?
+DROP TABLE customer_review;
+
+-- Find out how many rentals are still outstanding (i.e., have not been returned to the store yet).
+SELECT COUNT(*) AS outstanding_rentals
+FROM rental
+WHERE return_date IS NULL;
+-- Find the 30 most expensive movies which are outstanding (i.e., have not been returned yet).
+SELECT f.film_id, f.title, f.replacement_cost
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+WHERE r.return_date IS NULL
+GROUP BY f.film_id, f.title, f.replacement_cost
+ORDER BY f.replacement_cost DESC
+LIMIT 30;
+-- Your friend wants to rent 4 movies. Help find them based on these clues:
+-- .1. The 1st film: about a sumo wrestler, one of the actors is Penelope Monroe.
+SELECT f.film_id, f.title
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+JOIN actor a ON fa.actor_id = a.actor_id
+WHERE (f.description LIKE '%sumo wrestler%' OR f.title LIKE '%sumo wrestler%')
+AND a.first_name = 'Penelope' AND a.last_name = 'Monroe';
+-- .2. The 2nd film: A short documentary (less than 1 hour long), rated “R”.
+SELECT film_id, title
+FROM film
+WHERE length < 60
+AND rating = 'R'
+AND category_id = (SELECT category_id FROM category WHERE name = 'Documentary');
+
+SELECT f.film_id, f.title
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE f.length < 60
+AND f.rating = 'R'
+AND c.name = 'Documentary';
+-- 3. The 3rd film: Rented by Matthew Mahan, paid over $4.00, returned between 28th July and 1st August 2005.
+SELECT f.film_id, f.title
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN payment p ON r.rental_id = p.rental_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+WHERE c.first_name = 'Matthew' AND c.last_name = 'Mahan'
+AND p.amount > 4.00
+AND r.return_date BETWEEN '2005-07-28' AND '2005-08-01';
+
+-- 4. The 4th film: Also watched by Matthew Mahan, has the word "boat" in the title or description, and was expensive to replace.
+SELECT f.film_id, f.title, f.replacement_cost
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+WHERE c.first_name = 'Matthew' AND c.last_name = 'Mahan'
+AND (f.title LIKE '%boat%' OR f.description LIKE '%boat%')
+ORDER BY f.replacement_cost DESC
+LIMIT 1;
