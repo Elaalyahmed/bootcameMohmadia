44a78d89f496788ce928683c4989626968ad7fba
ahmed1998 <nextelaaly6@gmail.com>
2025-05-28 11:09:20 +0100

    update

---
+++ week3/day2/exersiceGold/ex1.js
@@ -0,0 +1,27 @@
+// // exersice1
+
+// let numbers = [123, 8409, 100053, 333333333, 7];
+
+// numbers.forEach(function(number) {
+//   console.log(number % 3 === 0);
+// });
+
+// // exersise2
+// let guestList = {
+//   randy: "Germany",
+//   karla: "France",
+//   wendy: "Japan",
+//   norman: "England",
+//   sam: "Argentina"
+// }
+
+// let name = prompt("What is your name?");
+//  if (guestList[name]) {
+//   console.log(`Hi! I'm ${name}, and I'm from ${guestList[name]}.`);
+// } else {
+//   console.log("Hi! I'm a guest.");
+// }
+// exersise3
+let age = [20,5,12,43,98,55];
+console.log(age)
+console.log(age[4])
